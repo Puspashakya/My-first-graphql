4f2148ea4909a403cab6f7a404562fddc4b2971d
puspa shakya <84306848+Puspashakya@users.noreply.github.com>
2022-01-23 12:44:30 +0545

    Add files via upload

---
+++ schema.py
@@ -0,0 +1,15 @@
+import graphene 
+from graphene_django import DjangoObjectType
+from .models import Books
+
+class BookType(DjangoObjectType):
+    class Meta:
+        model = Books
+        fields = ("id", "title", "excerpt")
+
+
+class Query(graphene.ObjectType):
+
+    all_books= graphene.List(BookType)
+
+schema = graphene.Schema(query=Query)
